402dd4132877c8cf33e523a2ff6bb3584b317623
Ethan <ethan_gustafson@outlook.com>
2020-02-04 15:51:23 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Category.create(name: "Drama")
+Category.create(name: "Comedy")
+Post.create(title: "How to Tame Your Dragon", content: "You do it like this....")
+Post.create(title: "Finished BoJack Horseman", content: "Got all kinds of feelings...")
+Post.create(title: "My Opinion on the Game of Thrones Ending", content: "It's whack! >:(")
+PostCategory.create(post_id: 1, category_id: 1)
+PostCategory.create(post_id: 1, category_id: 2)
+PostCategory.create(post_id: 2, category_id: 1)
+PostCategory.create(post_id: 3, category_id: 2)
+
+User.create(username: "picklerick", email: "imapickle@outlook.com")
+User.create(username: "jonsnow", email: "idoknow@yahoo.com")
+User.create(username: "frozone", email: "ifoundmysupersuit@aol.com")
